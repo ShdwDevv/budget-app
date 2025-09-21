1b7459f11ee4f7674c54a6499d15daedb3dacb6f
ShdwDevv <arshathahamed500@gmail.con>
2025-09-22 01:18:41 +0530

    Removed Loading

--- app/(routes)/dashboard/_components/CardInfo.jsx
+++ app/(routes)/dashboard/_components/CardInfo.jsx
@@ -20,37 +20,29 @@ function CardInfo({budgetList}) {
     }
     return (
         <div>
-            {budgetList.length > 0 ? 
-                <div className='mt-7 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5'>
-                    <div className='p-7 border rounded-lg flex items-center justify-between'>
-                        <div>
-                            <h2 className='text-sm'>Total Budget</h2>
-                            <h2 className='font-bold text-2xl'>₹{totalBudget}</h2>
-                        </div>
-                        <PiggyBank className='bg-primary p-3 h-12 w-12 rounded-full text-white'/>
+            <div className='mt-7 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5'>
+                <div className='p-7 border rounded-lg flex items-center justify-between'>
+                    <div>
+                        <h2 className='text-sm'>Total Budget</h2>
+                        <h2 className='font-bold text-2xl'>₹{totalBudget}</h2>
                     </div>
-                    <div className='p-7 border rounded-lg flex items-center justify-between'>
-                        <div>
-                            <h2 className='text-sm'>Total Spent</h2>
-                            <h2 className='font-bold text-2xl'>₹{totalSpend}</h2>
-                        </div>
-                        <ReceiptText className='bg-primary p-3 h-12 w-12 rounded-full text-white'/>
-                    </div>
-                    <div className='p-7 border rounded-lg flex items-center justify-between'>
-                        <div>
-                            <h2 className='text-sm'>No Of Budget</h2>
-                            <h2 className='font-bold text-2xl'>{budgetList?.length}</h2>
-                        </div>
-                        <Wallet className='bg-primary p-3 h-12 w-12 rounded-full text-white'/>
-                    </div>
+                    <PiggyBank className='bg-primary p-3 h-12 w-12 rounded-full text-white'/>
                 </div>
-                :
-                <div className='mt-7 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5'>
-                    {[1,2,3].map((item,index)=>(
-                        <div key={index} className='h-[120px] w-full bg-slate-200 anime-spin rounded-lg'></div>
-                    ))}
+                <div className='p-7 border rounded-lg flex items-center justify-between'>
+                    <div>
+                        <h2 className='text-sm'>Total Spent</h2>
+                        <h2 className='font-bold text-2xl'>₹{totalSpend}</h2>
+                    </div>
+                    <ReceiptText className='bg-primary p-3 h-12 w-12 rounded-full text-white'/>
                 </div>
-            }
+                <div className='p-7 border rounded-lg flex items-center justify-between'>
+                    <div>
+                        <h2 className='text-sm'>No Of Budget</h2>
+                        <h2 className='font-bold text-2xl'>{budgetList?.length}</h2>
+                    </div>
+                    <Wallet className='bg-primary p-3 h-12 w-12 rounded-full text-white'/>
+                </div>
+            </div>
         </div>
         
     )
--- app/(routes)/dashboard/budgets/_components/BudgetList.jsx
+++ app/(routes)/dashboard/budgets/_components/BudgetList.jsx
@@ -30,12 +30,9 @@ function BudgetList() {
         <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5'>
             <CreateBudget
             refreshData={()=>getBudgetList()} />
-            {budgetList?.length > 0 ? budgetList.map((budget,index) => (
+            {budgetList.map((budget,index) => (
               <BudgetItem key={budget.id} budget={budget} />
-            )): [1,2,3,4,5,6].map((item,index)=>(
-              <div key={index} className='w-full bg-slate-200 rounded-lg h-[150px] animate-pulse'></div>
-             ))
-          }
+            ))}
         </div>
     </div>
   )
