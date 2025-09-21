ce861feb57f7e43864900d5aad73a7ee96a8037c
ShdwDevv <arshathahamed500@gmail.con>
2025-09-21 17:41:27 +0530

    Added code for delete budget

--- app/(routes)/dashboard/budgets/_components/BudgetItem.jsx
+++ app/(routes)/dashboard/budgets/_components/BudgetItem.jsx
@@ -8,7 +8,7 @@ function BudgetItem({budget}) {
         return perc.toFixed(2);
     }
   return (
-    <Link href={'/dashboard/expenses/'+budget?.id} className='p-5 border rounded-lg hover:shadow-md cursor-pointer h-[170px]'>
+    <Link href={'/dashboard/expenses/'+budget?.id} className='p-5 border rounded-lg hover:shadow-md cursor-pointer h-[140px]'>
         <div className='flex gap-2 items-center justify-between'>
             <div className='flex gap-2 items-center'>
                 <h2 className='text-2xl p-2 bg-slate-100 rounded-full'>{budget?.icon}</h2>
@@ -22,7 +22,7 @@ function BudgetItem({budget}) {
         <div className='mt-5'>
             <div className='flex items-center justify-between mb-3'>
                 <h2 className='text-xs text-slate-400 '>
-                    ${budget.totalSpend?budget.totalSpend:20} Spend
+                    ${budget.totalSpend?budget.totalSpend:0} Spend
                 </h2>
                 <h2 className='text-xs text-slate-400 '>
                     ${budget.amount - budget.totalSpend} Remaining
