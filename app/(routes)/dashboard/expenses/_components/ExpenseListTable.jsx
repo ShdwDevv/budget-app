d4f06240c6f287880b4b64cd519a212c3cfbdbc9
ShdwDevv <arshathahamed500@gmail.con>
2025-09-22 01:36:59 +0530

    Fix Expense Table Alignment

--- app/(routes)/dashboard/expenses/_components/ExpenseListTable.jsx
+++ app/(routes)/dashboard/expenses/_components/ExpenseListTable.jsx
@@ -18,20 +18,21 @@ function ExpenseListTable({expensesList,refreshData}) {
         <div className='mt-3'>
             <h2 className='font-bold text-lg'>Latest Expenses</h2>
             <div className='grid grid-cols-4 bg-slate-200 p-2 mt-3'>
-                <h2 className='font-bold'>Name</h2>
-                <h2 className='font-bold'>Amount</h2>
-                <h2 className='font-bold'>Date</h2>
-                <h2 className='font-bold'>Action</h2> 
+                <h2 className='font-bold text-center'>Name</h2>
+                <h2 className='font-bold text-center'>Amount</h2>
+                <h2 className='font-bold text-center'>Date</h2>
+                <h2 className='font-bold text-center'>Action</h2> 
+                
             </div>
             {expensesList.map((expenses,index)=>(
                 <div key={expenses.id} className='grid grid-cols-4 bg-slate-100 p-2'>
-                <h2>{expenses.name}</h2>
-                <h2>{expenses.amount}</h2>
-                <h2>{expenses.createdAt}</h2>
-                <h2>
+                <h2 className='text-center'>{expenses.name}</h2>
+                <h2 className='text-center'>{expenses.amount}</h2>
+                <h2 className='text-center'>{expenses.createdAt}</h2>
+                <h2 className='flex justify-center'>
                 <AlertDialog>
                     <AlertDialogTrigger asChild>
-                        <Trash className='text-red-600 cursor-pointer ml-3'/>
+                        <Trash className='text-red-600 cursor-pointer'/>
                     </AlertDialogTrigger>
                     <AlertDialogContent>
                         <AlertDialogHeader>
